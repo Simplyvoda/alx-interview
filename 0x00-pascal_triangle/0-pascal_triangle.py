d50e68505c8fba706ae633911d8ea62514430542
Simplyvoda <efem.vodina@gmail.com>
2023-12-03 00:52:42 +0000

    Initial commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,11 @@
+def pascal_triangle(n):
+  # Check if n is less than or equal to 0
+  if n <= 0:
+      return []
+
+  # Create an empty list to store the triangle
+  triangle = []
+
+  # Generate Pascal's triangle logic here
+
+  return triangle
