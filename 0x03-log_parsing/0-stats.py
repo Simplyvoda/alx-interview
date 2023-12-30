973406afe6fa82ac6282a8d86dcfef33734ffb54
Vodina Efem <efem.vodina@gmail.com>
2023-12-30 23:49:25 +0000

    Fixes

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -15,38 +15,40 @@ pattern = '{}\\-{}{}{}{}\\s*'.format(fmt[0], fmt[1], fmt[2], fmt[3], fmt[4])
 
 
 def main():
-  line_count = 0
-  status_code = {}
-  total_size = 0
-
-  try:
-    for line in sys.stdin:
-      line = line.strip()  # remove newline character
-      match = re.fullmatch(pattern, line)  # check for match
-      if not match:
-        continue  # Move to the next line if there's no match
-
-      line_count += 1  # increase line count to perform print at 10counts
-      arguments = line.strip().split(" ")
-      total_size += int(arguments[-1])
-      code = arguments[-2]
-
-      if code in status_code:
-        status_code[code] += 1
-      else:
-        status_code[code] = 1
-
-      if line_count == 10:
-        print(f"File size: {total_size}")
+    line_count = 0
+    status_code = {}
+    total_size = 0
+    
+    
+    try:
+        for line in sys.stdin:
+            line = line.strip()  # remove newline character
+            match = re.fullmatch(pattern, line)  # check for match
+            if not match:
+                continue  # Move to the next line if there's no match
+            
+            line_count += 1  # increase line count to perform print at 10counts
+            arguments = line.strip().split(" ")
+            total_size += int(arguments[-1])
+            code = arguments[-2]
+            
+            if code in status_code:
+                status_code[code] += 1
+            else:
+                status_code[code] = 1
+                
+            if line_count == 10:
+                print(f"File size: {total_size}", flush=True)
+                
+                for status in sorted(status_code.keys()):
+                    print(f"{status}: {status_code[status]}", flush=True)
+                    
+                line_count = 0  # reset line count after 10th line
+    
+    except KeyboardInterrupt:
+        print(f"File size: {total_size}", flush=True)
         for status in sorted(status_code.keys()):
-          print(f"{status}: {status_code[status]}")
-        line_count = 0  # reset line count after 10th line
-
-  except KeyboardInterrupt:
-    print(f"File size: {total_size}")
-    for status in sorted(status_code.keys()):
-      print(f"{status}: {status_code[status]}")
-    sys.exit()
+            print(f"{status}: {status_code[status]}", flush=True)
 
 
 if __name__ == '__main__':
